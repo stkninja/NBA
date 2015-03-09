6a3b8b60d33598571e0d7ef800673a9b44412b70
qq <839889341@qq.com>
2015-03-09 20:06:29 +0800

    orgin

---
+++ src/po/TeamPO.java
@@ -0,0 +1,38 @@
+package po;
+
+public class TeamPO {
+	/**球队基本信息*/
+	//球队全名
+	private String fullName;
+	//球队缩写
+	private String abbName;
+	//所在地
+	private String location;
+	//赛区
+	private String competionArea;
+	//分区
+	private String subArea;
+	//主场
+	private String homeGround;
+	//建立时间
+	private int setupTime;
+	
+	/**球队比赛信息*/
+	//比赛场数
+	//投篮命中数
+	//投篮出手数
+	//三分命中数
+	//三分出手数
+	//罚球命中数
+	//罚球出手数
+	//进攻篮板数
+	//防守篮板数
+	//篮板数
+	//助攻数
+	//抢断数
+	//盖帽数
+	//
+	public TeamPO() {
+	}
+
+}
