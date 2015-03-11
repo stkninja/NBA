c38f720637bd2df8ef4ed983957f1c03c3607fd0
zk <673876747@qq.com>
2015-03-11 20:32:03 +0800

    zk

--- src/vo/PlayerVO.java
+++ src/vo/PlayerVO.java
@@ -1,7 +1,6 @@
 package vo;
 
 import java.awt.Image;
-import java.util.ArrayList;
 
 public class PlayerVO {
 	public String name;
@@ -10,36 +9,85 @@ public class PlayerVO {
 	public String team;
 	public String position;
 	public String subArea;                  //分区
-	public ArrayList<String> gameplay;      //参赛场数
-	public ArrayList<String> gamestart;     //先发场数
-	public ArrayList<String> rebound;       //篮板数
-	public ArrayList<String> assist;        //助攻数
-	public ArrayList<String> minute;        //在场时间
-	public ArrayList<String> offense;       //进攻数
-	public ArrayList<String> defence;       //防守数
-	public ArrayList<String> steal;         //抢断数
-	public ArrayList<String> block;         //盖帽数
-	public ArrayList<String> error;         //失误数
-	public ArrayList<String> foul;          //犯规数
-	public ArrayList<String> point;         //得分
-	public ArrayList<String> shoot;         //投篮数
-	public ArrayList<String> threepoint;    //三分球
-	public ArrayList<String> freethrow;     //罚球数
-	public ArrayList<String> doubledouble;  //两双
-	public ArrayList<String> fieldgoalpercent;  //投篮命中率
-	public ArrayList<String> threepointpercent; //三分命中率
-	public ArrayList<String> freethrowpercent;  //罚球命中率
-	public ArrayList<String> efficiency;    //效率
-	public ArrayList<String> gmsc;          //GmSc效率值
-	public ArrayList<String> realshootpercent;  //真实命中率
-	public ArrayList<String> shootefficiency;   //投篮效率
-	public ArrayList<String> reboundrate;       //篮板率
-	public ArrayList<String> offensivereboundrate;  //进攻篮板率
-	public ArrayList<String> defensivereboundrate;  //防守篮板率
-	public ArrayList<String> assistrate;        //助攻率
-	public ArrayList<String> stealrate;     //抢断率
-	public ArrayList<String> blockrate;     //盖帽率
-	public ArrayList<String> errorrate;     //失误率
-	public ArrayList<String> usage;         //使用率
+	public int gameplay;                    //参赛场数
+	public int gamestart;                   //先发场数
+	public int allrebound;                  //总篮板数
+	public double rebound;                  //场均篮板数
+	public double alloffensiverebound;      //总进攻篮板数
+	public double offensiverebound;         //场均进攻篮板数
+	public double alldefensiverebound;      //总防守篮板数
+	public double defensiverebound;         //场均防守篮板数
+	public int allassist;                   //总助攻数
+	public double assist;                   //场均助攻数
+	public double allminute;                //总在场时间
+	public double minute;                   //场均在场时间
+	public int alloffense;                  //总进攻数
+    public double offense;                  //场均进攻数
+	public int alldefence;                  //总防守数
+	public double defence;                  //场均防守数
+	public int allsteal;                    //总抢断数
+	public double steal;                    //场均抢断数
+	public int allblock;                    //总盖帽数
+	public double block;                    //场均盖帽数
+	public int allerror;                    //总失误数
+	public double error;                    //场均失误数
+	public int allfoul;                     //总犯规数
+	public double foul;                     //场均犯规数
+	public int allpoint;                    //总得分
+	public double point;                    //场均得分
+	public int allshoot;                    //总投篮出手数
+	public double shoot;                    //场均投篮出手数
+	public double allshootmade;             //总投篮命中数
+	public double shootmade;                //场均投篮命中数
+	public int allthreepoint;               //总三分出手数
+	public double threepoint;               //场均三分出手数
+	public double allthreepointmade;        //总三分命中数
+	public double threepointmade;           //场均三分命中数
+	public int allfreethrow;                //总罚球出手数
+	public double freethrow;                //场均罚球出手数
+	public double allfreethrowmade;         //总罚球命中数
+	public double freethrowmade;            //场均罚球命中数
+	public int doubledouble;                //两双
+	public double allfieldgoalpercent;      //总投篮命中率
+	public double fieldgoalpercent;         //场均投篮命中率
+	public double allthreepointpercent; //总三分命中率
+	public double threepointpercent; //场均三分命中率
+	public double allfreethrowpercent;  //总罚球命中率
+	public double freethrowpercent;  //场均罚球命中率
+	public double allefficiency;    //总效率
+	public double efficiency;    //场均效率
+	public double allgmsc;          //总GmSc效率值
+	public double gmsc;          //场均GmSc效率值
+	public double allrealshootpercent;  //总真实命中率
+	public double realshootpercent;  //场均真实命中率
+	public double allshootefficiency;   //总投篮效率
+	public double shootefficiency;   //场均投篮效率
+	public double allreboundrate;       //总篮板率
+	public double reboundrate;       //场均篮板率
+	public double alloffensivereboundrate;  //总进攻篮板率
+	public double offensivereboundrate;  //场均进攻篮板率
+	public double alldefensivereboundrate;  //总防守篮板率
+	public double defensivereboundrate;  //场均防守篮板率
+	public double allassistrate;        //总助攻率
+	public double assistrate;        //场均助攻率
+	public double allstealrate;     //总抢断率
+	public double stealrate;     //场均抢断率
+	public double allblockrate;     //总盖帽率
+	public double blockrate;     //场均盖帽率
+	public double allerrorrate;     //总失误率
+	public double errorrate;     //场均失误率
+	public double allusage;         //总使用率
+	public double usage;         //场均使用率
 	
+	public PlayerVO(){
+		allfieldgoalpercent = allshootmade / allshoot;
+		allthreepointpercent = allthreepointmade / allthreepoint;
+		allfreethrowpercent = allfreethrowmade / allfreethrow;
+		allefficiency = (allpoint + allrebound + allassist + allsteal + allblock) - (allshoot - allshootmade) - (allfreethrow - allfreethrowmade) - allerror;
+		allgmsc = allpoint + 0.4 * allshootmade - 0.7 * allshoot - 0.4 * (allfreethrow - allfreethrowmade) + 0.7 * alloffensiverebound + 0.3 * alldefensiverebound + allsteal + 0.7 * allassist + 0.7 * allblock - 0.4 * allfoul - allerror;
+		allrealshootpercent = allpoint / (2 * (allshoot + 0.44 * allfreethrow));
+		allshootefficiency = (allshootmade + 0.5 * allthreepointmade) / allshoot;
+		allerrorrate = allerror / (allshoot - allthreepoint + 0.44 * allfreethrow + allerror);
+	}
 }
+
--- src/vo/TeamVO.java
+++ src/vo/TeamVO.java
@@ -1,7 +1,7 @@
 package vo;
 
 import java.awt.Image;
-import java.util.ArrayList;
+
 
 public class TeamVO {
 	/**球队基本信息*/
@@ -27,137 +27,122 @@ public class TeamVO {
 	public double gamesNum;
 	//胜利场数
 	public double winsNum;
-	//赛季总、场均投篮命中数
-	public ArrayList<String> shootingHit = new ArrayList<String>();
-	//赛季总、场均投篮出手数
-	public ArrayList<String> shooting = new ArrayList<String>();
-	//赛季总、场均三分命中数
-	public ArrayList<String> threePointHits = new ArrayList<String>();
-	//赛季总、场均三分出手数
-	public ArrayList<String> threePoint = new ArrayList<String>();
-	//赛季总、场均罚球命中数
-	public ArrayList<String> freeThrowHit = new ArrayList<String>();
-	//赛季总、场均罚球出手数
-	public ArrayList<String> freeThrow = new ArrayList<String>();
-	//赛季总、场均进攻篮板数
-	public ArrayList<String> offensiveRebounds = new ArrayList<String>();
-	//赛季总、场均防守篮板数
-	public ArrayList<String> defensiveRebounds = new ArrayList<String>();
-	//赛季总、场均对手进攻篮板数
-	public ArrayList<String> opponentOffensiveRebounds = new ArrayList<String>();
-	//赛季总、场均对手防守篮板数
-	public ArrayList<String> opponentDefensiveRebounds = new ArrayList<String>();
-	//赛季总、场均篮板数
-	public ArrayList<String> rebounds = new ArrayList<String>();
-	//赛季总、场均助攻数
-	public ArrayList<String> assists = new ArrayList<String>();
-	//赛季总、场均抢断数
-	public ArrayList<String> steals = new ArrayList<String>();
-	//赛季总、场均盖帽数
-	public ArrayList<String> caps = new ArrayList<String>();
-	//赛季总、场均失误数
-	public ArrayList<String> turnovers = new ArrayList<String>();
-	//赛季总、场均犯规数
-	public ArrayList<String> fouls = new ArrayList<String>();
-	//赛季总、场均比赛得分
-	public ArrayList<String> scores = new ArrayList<String>();
-	//赛季总、场均投篮命中率
-	public ArrayList<String> shootingHitRate = new ArrayList<String>();
-	//赛季总、场均罚球命中率
-	public ArrayList<String> freeThrowHitRate = new ArrayList<String>();
-	//赛季总、场均三分命中率
-	public ArrayList<String> threePointHitRate = new ArrayList<String>();
+	//赛季总投篮命中数
+	public double allshootingHit;
+	//赛季场均投篮命中数
+    public double shootingHit;
+	//赛季总投篮出手数
+	public double allshooting;
+	//赛季场均投篮出手数
+	public double shooting;
+	//赛季总三分命中数
+	public double allthreePointHits;
+	//赛季场均三分命中数
+	public double threePointHits;
+	//赛季总三分出手数
+	public double allthreePoint;
+	//赛季场均三分出手数
+	public double threePoint;
+	//赛季总罚球命中数
+	public double allfreeThrowHit;
+	//赛季场均罚球命中数
+	public double freeThrowHit;
+	//赛季总罚球出手数
+	public double allfreeThrow;
+	//赛季场均罚球出手数
+	public double freeThrow;
+	//赛季总进攻篮板数
+	public double alloffensiveRebounds;
+	//赛季场均进攻篮板数
+	public double offensiveRebounds;
+	//赛季总防守篮板数
+	public double alldefensiveRebounds;
+	//赛季场均防守篮板数
+	public double defensiveRebounds;
+	//赛季总对手进攻篮板数
+	public double allopponentOffensiveRebounds;
+	//赛季场均对手进攻篮板数
+	public double opponentOffensiveRebounds;
+	//赛季总对手防守篮板数
+	public double allopponentDefensiveRebounds;
+	//赛季场均对手防守篮板数
+	public double opponentDefensiveRebounds;
+	//赛季总篮板数
+	public double allrebounds;
+	//赛季场均篮板数
+	public double rebounds;
+	//赛季总助攻数
+	public double allassists;
+	//赛季场均助攻数
+	public double assists;
+	//赛季总抢断数
+	public double allsteal;
+	//赛季场均抢断数
+	public double steal;
+	//赛季总盖帽数
+	public double allcaps;
+	//赛季场均盖帽数
+	public double caps;
+	//赛季总失误数
+	public double allturnovers;
+	//赛季场均失误数
+	public double turnovers;
+	//赛季总犯规数
+	public double allfouls;
+	//赛季场均犯规数
+	public double fouls;
+	//赛季总比赛得分
+	public double allscores;
+	//赛季场均比赛得分
+	public double scores;
+	//赛季总投篮命中率
+	public double allshootingHitRate;
+	//赛季场均投篮命中率
+	public double shootingHitRate;
+	//赛季总罚球命中率
+	public double allfreeThrowHitRate;
+	//赛季场均罚球命中率
+	public double freeThrowHitRate;
+	//赛季总三分命中率
+	public double allthreePointHitRate;
+	//赛季场均三分命中率
+	public double threePointHitRate;
 	//赛季胜率
 	public double winsRate;
-	//赛季总、场均进攻回合
-	public ArrayList<String> attackRound = new ArrayList<String>();
-	//赛季总、场均进攻效率
-	public ArrayList<String> attackEfficiency = new ArrayList<String>();
-	//赛季总、场均防守效率
-	public ArrayList<String> defenceEfficiency = new ArrayList<String>();
-	//赛季总、场均篮板效率
-	public ArrayList<String> reboundsEfficiency = new ArrayList<String>();
-	//赛季总、场均抢断效率
-	public ArrayList<String> stealEfficiency = new ArrayList<String>();
-	//赛季总、场均助攻效率
-	public ArrayList<String> assistEfficiency = new ArrayList<String>();
+	//赛季总进攻回合
+	public double allattackRound;
+	//赛季场均进攻回合
+	public double attackRound;
+	//赛季总进攻效率
+	public double allattackEfficiency;
+	//赛季场均进攻效率
+	public double attackEfficiency;
+	//赛季总防守效率
+	public double alldefenceEfficiency;
+	//赛季场均防守效率
+	public double defenceEfficiency;
+	//赛季总篮板效率
+	public double allreboundsEfficiency;
+	//赛季场均篮板效率
+	public double reboundsEfficiency;
+	//赛季总抢断效率
+	public double allstealEfficiency;
+	//赛季场均抢断效率
+	public double stealEfficiency;
+	//赛季总助攻效率
+	public double allassistEfficiency;
+	//赛季场均助攻效率
+	public double assistEfficiency;
 
-	public TeamVO(Image teamLogo, String fullName, String abbName,
-			String location, String competionArea, String subArea,
-			String homeGround, int setupTime, double gamesNum, double winsNum,
-			ArrayList<String> shootingHit, ArrayList<String> shooting,
-			ArrayList<String> threePointHits, ArrayList<String> threePoint,
-			ArrayList<String> freeThrowHit, ArrayList<String> freeThrow,
-			ArrayList<String> offensiveRebounds,
-			ArrayList<String> defensiveRebounds,
-			ArrayList<String> opponentOffensiveRebounds,
-			ArrayList<String> opponentDefensiveRebounds,
-			ArrayList<String> rebounds, ArrayList<String> assists,
-			ArrayList<String> steals, ArrayList<String> caps,
-			ArrayList<String> turnovers, ArrayList<String> fouls,
-			ArrayList<String> scores) {
-		this.initTeamInfo(teamLogo, fullName, abbName,
-				location, competionArea, subArea,
-				homeGround, setupTime);
-		this.initGamesInfo(gamesNum, winsNum,
-				shootingHit, shooting,
-				threePointHits, threePoint,
-				freeThrowHit, freeThrow,
-				offensiveRebounds,
-				defensiveRebounds,
-				opponentOffensiveRebounds,
-				opponentDefensiveRebounds,
-				rebounds, assists,
-				steals, caps,
-				turnovers, fouls,
-				scores);
-	}
-	
-	/**初始化球队信息*/
-	private void initTeamInfo(Image teamLogo, String fullName, String abbName,
-			String location, String competionArea, String subArea,
-			String homeGround, int setupTime){
-		this.teamLogo = teamLogo;
-		this.fullName = fullName;
-		this.abbName = abbName;
-		this.location = location;
-		this.competionArea = competionArea;
-		this.subArea = subArea;
-		this.homeGround = homeGround;
-		this.setupTime = setupTime;
+	public TeamVO(){
+		allshootingHitRate = allshootingHit / allshooting;
+		allfreeThrowHitRate = allfreeThrowHit / allfreeThrow;
+		allthreePointHitRate = allthreePointHits / allthreePoint;
+		winsRate = winsNum / gamesNum;
+		allattackEfficiency = allscores * 100 / allattackRound;
+		allassistEfficiency = allassists * 100 / allattackRound;
 	}
 	
 	/**初始化比赛信息*/
-	private void initGamesInfo(double gamesNum, double winsNum,
-			ArrayList<String> shootingHit, ArrayList<String> shooting,
-			ArrayList<String> threePointHits, ArrayList<String> threePoint,
-			ArrayList<String> freeThrowHit, ArrayList<String> freeThrow,
-			ArrayList<String> offensiveRebounds,
-			ArrayList<String> defensiveRebounds,
-			ArrayList<String> opponentOffensiveRebounds,
-			ArrayList<String> opponentDefensiveRebounds,
-			ArrayList<String> rebounds, ArrayList<String> assists,
-			ArrayList<String> steals, ArrayList<String> caps,
-			ArrayList<String> turnovers, ArrayList<String> fouls,
-			ArrayList<String> scores){
-		this.gamesNum = gamesNum;
-		this.winsNum = winsNum;
-		this.shootingHit = shootingHit;
-		this.shooting = shooting;
-		this.threePointHits = threePointHits;
-		this.threePoint = threePoint;
-		this.freeThrowHit = freeThrowHit;
-		this.freeThrow = freeThrow;
-		this.offensiveRebounds = offensiveRebounds;
-		this.defensiveRebounds = defensiveRebounds;
-		this.opponentOffensiveRebounds = opponentOffensiveRebounds;
-		this.opponentDefensiveRebounds = opponentDefensiveRebounds;
-		this.rebounds = rebounds;
-		this.assists = assists;
-		this.steals = steals;
-		this.caps = caps;
-		this.turnovers = turnovers;
-		this.fouls = fouls;
-		this.scores = scores;
-	}
+	
 }
