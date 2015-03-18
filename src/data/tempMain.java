4e381111780f32af2f02785ea1af24e0a88418d6
qq <839889341@qq.com>
2015-03-18 19:30:19 +0800

    qq

--- src/data/tempMain.java
+++ src/data/tempMain.java
@@ -1,6 +1,9 @@
 package data;
 
-import data.rwArrangedFiles.WritePOs;
+import java.util.ArrayList;
+
+import po.MatchPO;
+import dataservice.IMatch;
 
 
 
@@ -8,12 +11,10 @@ public class tempMain {
 	public static void main(String[] args) {
 		long time1 = System.currentTimeMillis();
 		
-		WritePOs.writePOs();
-		GetPlayersInfo getPlayersInfo = new GetPlayersInfo();
+		IMatch iMatch = new GetMatchesInfo();
+		ArrayList<MatchPO> matches = iMatch.getMatchesAboutPlayer("Carlos Delfino");
 		
 		long time2 = System.currentTimeMillis();
-		System.out.println(time2 - time1);
-		System.out.println(getPlayersInfo.getPlayerGameStart("Reggie Evans", "13-14"));
-		System.out.println(getPlayersInfo.getPlayerGamePlay("Reggie Evans", "13-14"));
+		System.out.println(matches.size());
 	}
 }
