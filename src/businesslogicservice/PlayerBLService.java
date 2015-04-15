affdfb28942e257cd7060270cc3e4e98bc33a81b
zk <673876747@qq.com>
2015-04-15 15:23:54 +0800

    zk

--- src/businesslogicservice/PlayerBLService.java
+++ src/businesslogicservice/PlayerBLService.java
@@ -7,11 +7,9 @@ import vo.PlayerBasicInfoVO;
 import vo.PlayerVO;
 
 public interface PlayerBLService {
-	public ArrayList<PlayerVO> getPlayers(String subArea,String position,String team);
+	public ArrayList<PlayerVO> getPlayers(String season,String subArea,String position,String team);
 	
 	public PlayerBasicInfoVO getOnePlayer(String name);
-	
-	public ArrayList<PlayerVO> getAllPlayers();
 
 	public ArrayList<MatchVO> getLastFiveMatches(String name);
 	
