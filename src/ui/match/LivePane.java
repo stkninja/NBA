7ded5d70390042614195e44b97f255cdfdf00f93
gsh <723784969@qq.com>
2015-06-14 21:02:13 +0800

    Signed-off-by: gsh <723784969@qq.com>

--- src/ui/match/LivePane.java
+++ src/ui/match/LivePane.java
@@ -10,6 +10,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Enumeration;
@@ -18,6 +19,7 @@ import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -30,10 +32,18 @@ import javax.swing.table.TableColumn;
 
 import org.apache.batik.transcoder.TranscoderException;
 
+import businesslogic.LiveBL;
+import businesslogicservice.LiveBLService;
 import ui.SvgUtil;
+import vo.LiveInfoVO;
 
 @SuppressWarnings("serial")
 public class LivePane extends JPanel implements Runnable{
+	boolean isShow = false;
+	Thread t;
+	LiveBLService lbl;
+	JComboBox<String> mode;
+	JPanel panel;
     JPanel panel1;  //标题panel
 	
     JPanel panel2;  //数据panel
@@ -87,7 +97,11 @@ public class LivePane extends JPanel implements Runnable{
 	JTable table;
 	JScrollPane sp;
 	Object[][] data;
-	public LivePane() throws IOException, TranscoderException {
+	public void init(){
+		lbl = new LiveBL();
+		LiveInfoVO vo = lbl.getLiveInfo();
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
 		//标题panel
 		panel1 = new JPanel();
 		panel1.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -120,15 +134,33 @@ public class LivePane extends JPanel implements Runnable{
 		panelA.setOpaque(false);
 		
 		logofile1 = new File("logofile");
-		logofile1.createNewFile();
+		try {
+			logofile1.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		SvgUtil.convertSvgFile2Png(new File("data/teams/GSW.svg"), logofile1);
-		logo1 = ImageIO.read(logofile1);
+		try {
+			SvgUtil.convertSvgFile2Png(new File("data/teams/GSW.svg"), logofile1);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (TranscoderException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			logo1 = ImageIO.read(logofile1);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		logoicon1 = new ImageIcon(logo1);
 		logoicon1.setImage(logoicon1.getImage().getScaledInstance(120,120,Image.SCALE_DEFAULT));		
 		JLabel Pic1 = new JLabel();
 		Pic1.setIcon(logoicon1);
-		score1 = new JLabel("102");
+		score1 = new JLabel(String.valueOf(vo.homeScore));
 		score1.setFont(new Font("宋体",Font.BOLD,29));	
 		score1.setForeground(Color.RED);
 		subpanelA1 = new JPanel();
@@ -139,83 +171,101 @@ public class LivePane extends JPanel implements Runnable{
 		
 		subpanelA3 = new JPanel();
 		subpanelA3.setOpaque(false);
-		subpanelA3.setLayout(new GridLayout(3,5));
+		subpanelA3.setLayout(new GridLayout(3,4));
 		subpanelA3.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
 		Font f1 = new Font("宋体",Font.BOLD,15);
 		first = new JLabel("第一节",JLabel.CENTER);
 		second = new JLabel("第二节",JLabel.CENTER);
 		third = new JLabel("第三节",JLabel.CENTER);
 		forth = new JLabel("第四节",JLabel.CENTER);
-		plus = new JLabel("加时",JLabel.CENTER);
+//		plus = new JLabel("加时",JLabel.CENTER);
 		first.setFont(f1);
 		second.setFont(f1);
 		third.setFont(f1);
 		forth.setFont(f1);
-		plus.setFont(f1);
+//		plus.setFont(f1);
 		first.setForeground(Color.BLACK);
 		second.setForeground(Color.BLACK);
 		third.setForeground(Color.BLACK);
 		forth.setForeground(Color.BLACK);
-		plus.setForeground(Color.BLACK);
+//		plus.setForeground(Color.BLACK);
 		Font f2 = new Font("宋体",Font.BOLD,12);
-		h1 = new JLabel("24",JLabel.CENTER);
-		h2 = new JLabel("18",JLabel.CENTER);
-		h3 = new JLabel("28",JLabel.CENTER);
-		h4 = new JLabel("12",JLabel.CENTER);
-		hp = new JLabel("0",JLabel.CENTER);
+		h1 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[0]),JLabel.CENTER);
+		h2 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[2]),JLabel.CENTER);
+		h3 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[4]),JLabel.CENTER);
+		h4 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[6]),JLabel.CENTER);
+//		hp = new JLabel("0",JLabel.CENTER);
 		
-		c1 = new JLabel("31",JLabel.CENTER);
-		c2 = new JLabel("23",JLabel.CENTER);
-		c3 = new JLabel("22",JLabel.CENTER);
-		c4 = new JLabel("27",JLabel.CENTER);
-		cp = new JLabel("0",JLabel.CENTER);
+		c1 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[1]),JLabel.CENTER);
+		c2 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[3]),JLabel.CENTER);
+		c3 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[5]),JLabel.CENTER);
+		c4 = new JLabel(String.valueOf(LiveInfoVO.matchrecord[7]),JLabel.CENTER);
+//		cp = new JLabel("0",JLabel.CENTER);
 		h1.setFont(f2);
 		h2.setFont(f2);
 		h3.setFont(f2);
 		h4.setFont(f2);
-		hp.setFont(f2);
+//		hp.setFont(f2);
 		c1.setFont(f2);
 		c2.setFont(f2);
 		c3.setFont(f2);
 		c4.setFont(f2);
-		cp.setFont(f2);
+//		cp.setFont(f2);
 		h1.setForeground(Color.MAGENTA);
 		h2.setForeground(Color.MAGENTA);
 		h3.setForeground(Color.MAGENTA);
 		h4.setForeground(Color.MAGENTA);
-		hp.setForeground(Color.MAGENTA);
+//		hp.setForeground(Color.MAGENTA);
 		c1.setForeground(Color.MAGENTA);
 		c2.setForeground(Color.MAGENTA);
 		c3.setForeground(Color.MAGENTA);
 		c4.setForeground(Color.MAGENTA);
-		cp.setForeground(Color.MAGENTA);
+//		cp.setForeground(Color.MAGENTA);
 		subpanelA3.add(first);
 		subpanelA3.add(second);
 		subpanelA3.add(third);
 		subpanelA3.add(forth);
-		subpanelA3.add(plus);
+//		subpanelA3.add(plus);
 		subpanelA3.add(h1);
 		subpanelA3.add(h2);
 		subpanelA3.add(h3);
 		subpanelA3.add(h4);
-		subpanelA3.add(hp);
+//		subpanelA3.add(hp);
 		subpanelA3.add(c1);
 		subpanelA3.add(c2);
 		subpanelA3.add(c3);
 		subpanelA3.add(c4);
-		subpanelA3.add(cp);
+//		subpanelA3.add(cp);
 		
 		
 		logofile2 = new File("logofile");
-		logofile2.createNewFile();
+		try {
+			logofile2.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		SvgUtil.convertSvgFile2Png(new File("data/teams/CLE.svg"), logofile2);
-		logo2 = ImageIO.read(logofile2);
+		try {
+			SvgUtil.convertSvgFile2Png(new File("data/teams/CLE.svg"), logofile2);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (TranscoderException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			logo2 = ImageIO.read(logofile2);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		logoicon2 = new ImageIcon(logo2);
 		logoicon2.setImage(logoicon2.getImage().getScaledInstance(120,120,Image.SCALE_DEFAULT));		
 		JLabel Pic2 = new JLabel();
 		Pic2.setIcon(logoicon2);
-		score2 = new JLabel("103");
+		score2 = new JLabel(String.valueOf(vo.visitScore));
 		score2.setFont(new Font("宋体",Font.BOLD,29));
 		score2.setForeground(Color.RED);
 		subpanelA2 = new JPanel();
@@ -236,19 +286,27 @@ public class LivePane extends JPanel implements Runnable{
 		panelB.setBorder(BorderFactory.createEmptyBorder(0, 12, 10, 12));
 		subpanelB1 = new JPanel();
 		subpanelB1.setLayout(new FlowLayout(FlowLayout.LEFT));
-		subtitle = new JLabel("直播实况         ");
+		subtitle = new JLabel("直播实况 ");
 		subtitle.setFont(new Font("楷体",Font.BOLD,20));
 		refresh = new JButton("刷新");
+		mode = new JComboBox<String>(new String[]{"自动刷新","手动刷新" });
+		mode.addActionListener(
+				new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					thread();
+				}
+				});
 		subpanelB1.add(subtitle);
+		subpanelB1.add(mode);
 		subpanelB1.add(refresh);
 		refresh.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				setData(); 
+				refresh(); 
 			}
 		});
 		table = new JTable();
 		sp = new JScrollPane(table);
-		this.setData(); 
+		this.setData(vo); 
 		
 		panelB.add(subpanelB1,BorderLayout.NORTH);
 		panelB.add(sp,BorderLayout.CENTER);
@@ -259,21 +317,78 @@ public class LivePane extends JPanel implements Runnable{
 		panel2.add(panelA,BorderLayout.NORTH);
 		panel2.add(panelB,BorderLayout.CENTER);
 		
+		panel.add(panel1, BorderLayout.NORTH);
+		panel.add(panel2, BorderLayout.CENTER);
 		this.setLayout(new BorderLayout());
-		this.add(panel1, BorderLayout.NORTH);
-		this.add(panel2, BorderLayout.CENTER);
+		this.add(panel,BorderLayout.CENTER);
+		
+	}
+	public LivePane(){
+		isShow = true;
+		init();
+		t =new Thread(){
+			public void run() {
+				while(isShow){
+					
+					refresh();
+					System.out.println(isShow);
+					try {
+						Thread.sleep(5000);
+						} catch(InterruptedException e) {
+						return;
+						}
+				}
+			}
+		};
+		t.start();
+		
+	}
+	public void run() {
+		while(true){
+			
+			refresh();
+			System.out.println("0");
+			try {
+				Thread.sleep(5000);
+				} catch(InterruptedException e) {
+				return;
+				}
+		}
+	}
+	private void thread(){
+		if(mode.getSelectedItem().equals("手动刷新")){
+			t.interrupt();
+		}
+		else{
+			t.interrupt();
+			t =new Thread(){
+				public void run() {
+					while(isShow){
+						
+						refresh();
+						System.out.println(isShow);
+						try {
+							Thread.sleep(5000);
+							} catch(InterruptedException e) {
+							return;
+							}
+					}
+				}
+			};
+			t.start();
+		}
+		
 	}
 	
-	private void setData(){
-		data = new Object[50][4];
-		for(int n = 0; n < 50;n++){
-			data[n][0] = "12:00";
-			data[n][1] = "骑士";
-			data[n][2] = "莫兹戈夫和德雷蒙德-格林跳球，特里斯坦-汤普森得到篮球";
-			data[n][3] = "0-0";
-		}
+	private void setData(LiveInfoVO vo){
+		data = vo.live;
 		this.showTable(data);
 	}
+	
+	private void refresh(){
+		
+		this.repaint();
+	};
 	
 	private void showTable(Object[][] data) {
 		this.remove(table);
@@ -299,16 +414,7 @@ public class LivePane extends JPanel implements Runnable{
 		sp.setViewportView(table);
 		revalidate();
 	}
-	public void run() {
-		while(true){
-			this.setData();
-			try {
-				Thread.sleep(5000);
-				} catch(InterruptedException e) {
-				return;
-				}
-		}
-	}
+	
 		
 		
 	
@@ -341,9 +447,7 @@ public class LivePane extends JPanel implements Runnable{
 		int frameWidth = frameHeight * 13 / 10;
 		frame.setBounds((screenSize.width - frameWidth) / 2, (screenSize.height - frameHeight) / 2, frameWidth, frameHeight);
 		LivePane lp = new LivePane();
-		Thread t = new Thread(lp,"A");
 		frame.add(lp);
-		t.start();
 		frame.setVisible(true);
 	}
 
