f24ddd9006d16ccd18c58867878fe94424653201
stk <1332121007@qq.com>
2015-06-14 16:00:37 +0800

    Signed-off-by: stk <1332121007@qq.com>

--- src/ui/news/NewsList.java
+++ src/ui/news/NewsList.java
@@ -6,6 +6,8 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -80,9 +83,11 @@ public class NewsList extends JPanel {
 		top.setLayout(new BoxLayout(top, BoxLayout.X_AXIS));
 		top.setOpaque(false);
 		
-		button1 = new JButton("<");
+		button1 = new JButton();
+		this.setIcon(button1, "data/pic/back.png", "data/pic/back.png");
 		button1.setEnabled(false);
-		button2 = new JButton("O");
+		button2 = new JButton();
+		this.setIcon(button2, "data/pic/refresh.png", "data/pic/refresh.png");
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				card.show(pane, "list");
@@ -103,7 +108,9 @@ public class NewsList extends JPanel {
 		top.add(label);
 		top.add(Box.createHorizontalGlue());
 		top.add(button1);
+		top.add(Box.createHorizontalStrut(10));
 		top.add(button2);
+		top.add(Box.createHorizontalStrut(10));
 		this.add(top, BorderLayout.NORTH);
 	}
 	/**
@@ -171,5 +178,30 @@ public class NewsList extends JPanel {
 		});
 		
 		sp.setViewportView(table);
+	}
+	/**
+	 * 设置图标
+	 * @param button 图标
+	 * @param file1 默认图标路径
+	 * @param file2 翻转图标路径
+	 */
+	private void setIcon(JButton button, String file1, String file2) {
+		button.setPreferredSize(new Dimension(30, 30));
+		button.setSize(new Dimension(30, 30));
+        Image icon1 = (new ImageIcon(file1)).getImage();
+        double scale1 = (double)icon1.getWidth(null) / (double)icon1.getHeight(null);
+		Image temp1 = icon1.getScaledInstance((int)(button.getHeight() * scale1), button.getHeight(), Image.SCALE_DEFAULT);
+		Image icon2 = (new ImageIcon(file2)).getImage();
+		double scale2 = (double)icon2.getWidth(null) / (double)icon2.getHeight(null);
+		Image temp2 = icon2.getScaledInstance((int)(button.getHeight() * scale2), button.getHeight(), Image.SCALE_DEFAULT);
+        button.setIcon(new ImageIcon(temp1));
+		button.setRolloverIcon(new ImageIcon(temp2));
+		button.setPressedIcon(new ImageIcon(temp2));
+		button.setFocusPainted(false);//无选择效果
+        button.setOpaque(false);//透明
+		button.setContentAreaFilled(false);//填充
+		button.setBorderPainted(false);//无边框
+		button.setMargin(new Insets(0, 0, 0, 0));//无边距
+		button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));//指针变手
 	}
 }
